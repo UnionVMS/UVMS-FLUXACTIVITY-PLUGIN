4716b3de1f358df1116b2ef128ac240e33cd69c3
emsanerac <Rachana.Sane@arhs-developments.com>
2017-04-10 11:33:27 +0200

    UNIONVMS-2514 : connection to the FLUX Queue added

--- service/src/main/java/eu/europa/ec/fisheries/uvms/plugins/fluxActivity/constants/FluxPluginConstants.java
+++ service/src/main/java/eu/europa/ec/fisheries/uvms/plugins/fluxActivity/constants/FluxPluginConstants.java
@@ -7,8 +7,8 @@ public class FluxPluginConstants {
     private FluxPluginConstants() {
     }
 
-    public static final String FLUX_MESSAGE_IN_REMOTE_QUEUE_NAME = "FAQuery";
-    public static final String FLUX_MESSAGE_IN_REMOTE_QUEUE = "java:/jms/queue/FAQuery"; // This Queue has been configured on FLUX box. We will receive FAReportMessage in this queue.
+    public static  final String FLUX_MESSAGE_IN_REMOTE_QUEUE_NAME = "FAQuery";
+    public static  final String FLUX_MESSAGE_IN_REMOTE_QUEUE = "java:/jms/queue/FAQuery"; // This Queue has been configured on FLUX box. We will receive FAReportMessage in this queue.
     public static final String DESTINATION_TYPE_QUEUE = "javax.jms.Queue";
     public static final String FLUX_CONNECTION_FACTORY = "java:/FluxFactory";
 
---
+++ service/src/main/java/eu/europa/ec/fisheries/uvms/plugins/fluxActivity/consumer/FluxMessageConsumer.java
@@ -0,0 +1,53 @@
+package eu.europa.ec.fisheries.uvms.plugins.fluxActivity.consumer;
+
+
+import eu.europa.ec.fisheries.uvms.plugins.fluxActivity.constants.FluxPluginConstants;
+import eu.europa.ec.fisheries.uvms.plugins.fluxActivity.service.ExchangeService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.ejb.ActivationConfigProperty;
+import javax.ejb.EJB;
+import javax.ejb.MessageDriven;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.TextMessage;
+
+
+@MessageDriven(mappedName = FluxPluginConstants.FLUX_MESSAGE_IN_REMOTE_QUEUE_NAME,  activationConfig = {
+        @ActivationConfigProperty(propertyName = "destinationType", propertyValue = FluxPluginConstants.DESTINATION_TYPE_QUEUE),
+        @ActivationConfigProperty(propertyName = "destination", propertyValue = FluxPluginConstants.FLUX_MESSAGE_IN_REMOTE_QUEUE),
+        @ActivationConfigProperty(propertyName = "connectionFactoryLookup", propertyValue = FluxPluginConstants.FLUX_CONNECTION_FACTORY)
+})
+public class FluxMessageConsumer implements MessageListener {
+
+    final static Logger LOG = LoggerFactory.getLogger(FluxMessageConsumer.class);
+
+    @EJB
+    ExchangeService exchange;
+
+
+    @Override
+    @TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
+    public void onMessage(Message inMessage) {
+
+        LOG.info("------Received FAReportMessage in ERS Activity plugin from FLUX--------");
+
+        TextMessage textMessage = (TextMessage) inMessage;
+
+        try {
+
+            LOG.debug("Received FAReportMessage :"+textMessage.getText());
+            exchange.sendFLUXFAReportMessageReportToExchange(textMessage.getText());
+            LOG.info("message sent successfully to exchange module");
+
+
+        } catch (Exception e) {
+            LOG.error("Error while trying to send Flux FAReport message to exchange",e);
+        }
+    }
+
+
+}
