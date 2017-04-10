57baf692cb197f2f825d6713008fa565228751dc
emsanerac <Rachana.Sane@arhs-developments.com>
2017-04-10 11:11:51 +0200

    UNIONVMS-2514: Made changes to point to correct queue

--- service/src/main/java/eu/europa/ec/fisheries/uvms/plugins/fluxActivity/constants/FluxPluginConstants.java
+++ service/src/main/java/eu/europa/ec/fisheries/uvms/plugins/fluxActivity/constants/FluxPluginConstants.java
@@ -3,16 +3,13 @@ package eu.europa.ec.fisheries.uvms.plugins.fluxActivity.constants;
 /**
  * Created by sanera on 27/05/2016.
  */
-public interface FluxPluginConstants {
-     String FLUX_MESSAGE_IN_QUEUE = "java:/jms/queue/ERSPlugin";
+public class FluxPluginConstants {
+    private FluxPluginConstants() {
+    }
 
-     String FLUX_MESSAGE_IN_QUEUE_NEW_NAME = "mdrin";
-     String FLUX_MESSAGE_IN_QUEUE_NEW = "jms/queue/mdrin";
-     String CONNECTION_TYPE = "javax.jms.MessageListener";
-     String DESTINATION_TYPE_QUEUE = "javax.jms.Queue";
-     String QUEUE_FLUX_RECEIVER_NAME = "ERSPlugin";
-     //String FLUX_MESSAGE_DESTINATION_QUEUE = "java:/jms/queue/ERSPlugin";
-     public static final String CONNECTION_FACTORY = "jms/RemoteConnectionFactory";
-     public static final String FLUX_CONNECTION_FACTORY = "java:/FluxFactory";
+    public static final String FLUX_MESSAGE_IN_REMOTE_QUEUE_NAME = "FAQuery";
+    public static final String FLUX_MESSAGE_IN_REMOTE_QUEUE = "java:/jms/queue/FAQuery"; // This Queue has been configured on FLUX box. We will receive FAReportMessage in this queue.
+    public static final String DESTINATION_TYPE_QUEUE = "javax.jms.Queue";
+    public static final String FLUX_CONNECTION_FACTORY = "java:/FluxFactory";
 
 }
--- service/src/main/java/eu/europa/ec/fisheries/uvms/plugins/fluxActivity/consumer/FluxMessageListner.java
+++ service/src/main/java/eu/europa/ec/fisheries/uvms/plugins/fluxActivity/consumer/FluxMessageListner.java
@@ -1,72 +1,53 @@
 package eu.europa.ec.fisheries.uvms.plugins.fluxActivity.consumer;
 
 
-import eu.europa.ec.fisheries.schema.exchange.registry.v1.ExchangeRegistryBaseRequest;
-import eu.europa.ec.fisheries.uvms.exchange.model.exception.ExchangeModelMarshallException;
-import eu.europa.ec.fisheries.uvms.exchange.model.mapper.JAXBMarshaller;
-import eu.europa.ec.fisheries.uvms.plugins.fluxActivity.StartupBean;
+import eu.europa.ec.fisheries.uvms.plugins.fluxActivity.constants.FluxPluginConstants;
 import eu.europa.ec.fisheries.uvms.plugins.fluxActivity.service.ExchangeService;
-import eu.europa.ec.fisheries.uvms.plugins.fluxActivity.service.PluginService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import un.unece.uncefact.data.standard.fluxfareportmessage._3.FLUXFAReportMessage;
 
+import javax.ejb.ActivationConfigProperty;
 import javax.ejb.EJB;
+import javax.ejb.MessageDriven;
 import javax.ejb.TransactionAttribute;
 import javax.ejb.TransactionAttributeType;
-import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.MessageListener;
 import javax.jms.TextMessage;
 
 
-/*@MessageDriven(mappedName = FluxPluginConstants.FLUX_MESSAGE_IN_QUEUE_NEW_NAME,  activationConfig = {
+@MessageDriven(mappedName = FluxPluginConstants.FLUX_MESSAGE_IN_REMOTE_QUEUE_NAME,  activationConfig = {
         @ActivationConfigProperty(propertyName = "destinationType", propertyValue = FluxPluginConstants.DESTINATION_TYPE_QUEUE),
-        @ActivationConfigProperty(propertyName = "destination", propertyValue = FluxPluginConstants.FLUX_MESSAGE_IN_QUEUE_NEW),
+        @ActivationConfigProperty(propertyName = "destination", propertyValue = FluxPluginConstants.FLUX_MESSAGE_IN_REMOTE_QUEUE),
         @ActivationConfigProperty(propertyName = "connectionFactoryLookup", propertyValue = FluxPluginConstants.FLUX_CONNECTION_FACTORY)
-})*/
+})
 public class FluxMessageListner implements MessageListener {
 
     final static Logger LOG = LoggerFactory.getLogger(FluxMessageListner.class);
 
     @EJB
     ExchangeService exchange;
-    @EJB
-    StartupBean startupService;
 
-    @EJB
-    PluginService fluxService;
 
     @Override
     @TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
     public void onMessage(Message inMessage) {
 
-        LOG.info("Rachana Flux plugin receiver got flux message");
+        LOG.info("------Received FAReportMessage in ERS Activity plugin from FLUX--------");
 
         TextMessage textMessage = (TextMessage) inMessage;
 
         try {
-           if(tryConsumeFAReportMessage(textMessage) ==null){
-               LOG.info("Unable to read the xml message. Incompatible message.");
-           }
-            LOG.info("message sent successfully");
+
+            LOG.debug("Received FAReportMessage :"+textMessage.getText());
             exchange.sendFLUXFAReportMessageReportToExchange(textMessage.getText());
+            LOG.info("message sent successfully to exchange module");
 
 
-        } catch (JMSException e) {
+        } catch (Exception e) {
             LOG.error("Error while trying to send Flux FAReport message to exchange",e);
-
         }
     }
 
 
-    private ExchangeRegistryBaseRequest tryConsumeFAReportMessage(TextMessage textMessage) {
-        try {
-
-            return JAXBMarshaller.unmarshallTextMessage(textMessage, FLUXFAReportMessage.class);
-        } catch (ExchangeModelMarshallException e) {
-            LOG.error("Error while trying read FLUXFA ReportEMssage. Format of xml is incorrect.",e);
-            return null;
-        }
-    }
 }
