eb86429b60afeb35e8c15a94140f55c6439e782f
ARIS-LUX\kovian <Andi.Kovi@arhs-developments.com>
2017-10-06 10:55:19 +0200

    UNIONVMS-3184 implementation Onvalue to the referencedId of the Response

--- service/src/main/java/eu/europa/ec/fisheries/uvms/plugins/fluxActivity/ExchangeMessageProperties.java
+++ service/src/main/java/eu/europa/ec/fisheries/uvms/plugins/fluxActivity/ExchangeMessageProperties.java
@@ -22,6 +22,7 @@ public class ExchangeMessageProperties {
     private String username;
     private String reportType;
     private String DFValue;
+    private String onValue;
     private Date date;
     private PluginType pluginType;
     private String senderReceiver;
@@ -30,55 +31,48 @@ public class ExchangeMessageProperties {
     public String getUsername() {
         return username;
     }
-
     public void setUsername(String username) {
         this.username = username;
     }
-
     public String getReportType() {
         return reportType;
     }
-
     public void setReportType(String reportType) {
         this.reportType = reportType;
     }
-
     public String getDFValue() {
         return DFValue;
     }
-
     public void setDFValue(String DFValue) {
         this.DFValue = DFValue;
     }
-
+    public String getOnValue() {
+        return onValue;
+    }
+    public void setOnValue(String onValue) {
+        this.onValue = onValue;
+    }
     public Date getDate() {
         return date;
     }
-
     public void setDate(Date date) {
         this.date = date;
     }
-
     public PluginType getPluginType() {
         return pluginType;
     }
-
     public void setPluginType(PluginType pluginType) {
         this.pluginType = pluginType;
     }
-
     public String getSenderReceiver() {
         return senderReceiver;
     }
-
     public void setSenderReceiver(String senderReceiver) {
         this.senderReceiver = senderReceiver;
     }
-
     public String getMessageGuid() {
         return messageGuid;
     }
-
     public void setMessageGuid(String messageGuid) {
         this.messageGuid = messageGuid;
     }
--- service/src/main/java/eu/europa/ec/fisheries/uvms/plugins/fluxActivity/service/ExchangeService.java
+++ service/src/main/java/eu/europa/ec/fisheries/uvms/plugins/fluxActivity/service/ExchangeService.java
@@ -13,92 +13,90 @@ import eu.europa.ec.fisheries.uvms.plugins.fluxActivity.constants.DataSourceQueu
 import eu.europa.ec.fisheries.uvms.plugins.fluxActivity.parser.SAXParserForFaFLUXMessge;
 import eu.europa.ec.fisheries.uvms.plugins.fluxActivity.parser.UUIDSAXException;
 import eu.europa.ec.fisheries.uvms.plugins.fluxActivity.producer.PluginMessageProducer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.xml.sax.SAXException;
-
+import java.util.Date;
 import javax.ejb.EJB;
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
 import javax.jms.JMSException;
 import javax.jms.TextMessage;
-import java.util.Date;
+import lombok.extern.slf4j.Slf4j;
+import org.xml.sax.SAXException;
 
 /**
- *
  * @author jojoha
  */
 @LocalBean
 @Stateless
+@Slf4j
 public class ExchangeService {
 
-    private static final String exchangeUsername ="flux";
-    private static final String DF ="DF";
-    private static final String FR ="FR";
-    private static final Logger LOG = LoggerFactory.getLogger(ExchangeService.class);
+    private static final String exchangeUsername = "flux";
+    private static final String DF = "DF";
+    private static final String FR = "FR";
+    private static final String ON = "ON";
 
     @EJB
-   // PluginMessageProducer producer;
-            PluginMessageProducer producer;
+    PluginMessageProducer producer;
 
     public void sendFLUXFAReportMessageReportToExchange(String fluxFAReportMessage, ExchangeMessageProperties prop) {
-       try {
-           LOG.info("Prepare FLUXFAReportMessageRequest to send to exchange");
-            String text = ExchangeModuleRequestMapper.createFluxFAReportRequest(fluxFAReportMessage,prop.getUsername()
-                    ,prop.getDFValue(),prop.getDate(),prop.getMessageGuid(),prop.getPluginType(),prop.getSenderReceiver());
-           LOG.info("Exchange request created :"+text);
-           String messageId = producer.sendModuleMessage(text, DataSourceQueue.EXCHANGE);
-           LOG.info("Message sent to exchange module :"+messageId);
+        try {
+            log.info("[START] Preparing FLUXFAReportMessageRequest to send to exchange");
+            String text = ExchangeModuleRequestMapper.createFluxFAReportRequest(fluxFAReportMessage, prop.getUsername()
+                    , prop.getDFValue(), prop.getDate(), prop.getMessageGuid(), prop.getPluginType(), prop.getSenderReceiver(), prop.getOnValue());
+            log.info("[START] Exchange request created :" + text);
+            String messageId = producer.sendModuleMessage(text, DataSourceQueue.EXCHANGE);
+            log.info("[END] Message sent to exchange module :" + messageId);
         } catch (ExchangeModelMarshallException e) {
-            LOG.error("Couldn't create FluxFAReportRequest for Exchange",e);
-        }  catch (JMSException e) {
-           LOG.error("Couldn't send FluxFAReportRequest to Exchange",e);
+            log.error("Couldn't create FluxFAReportRequest for Exchange", e);
+        } catch (JMSException e) {
+            log.error("Couldn't send FluxFAReportRequest to Exchange", e);
 
-       }
+        }
     }
 
     /**
      * Create object with all necessary properties required to communicate with exchange
+     *
      * @param textMessage
      * @return
      * @throws JMSException
      */
-     public ExchangeMessageProperties createExchangeMessagePropertiesForFluxFAReportRequest(TextMessage textMessage) throws JMSException {
+    public ExchangeMessageProperties createExchangeMessagePropertiesForFluxFAReportRequest(TextMessage textMessage) throws JMSException {
         ExchangeMessageProperties exchangeMessageProperties = new ExchangeMessageProperties();
         exchangeMessageProperties.setUsername(exchangeUsername);
         exchangeMessageProperties.setDate(new Date());
         exchangeMessageProperties.setPluginType(PluginType.FLUX);
-        exchangeMessageProperties.setDFValue(extractStringPropertyFromJMSTextMessage(textMessage,DF));
-        exchangeMessageProperties.setSenderReceiver(extractStringPropertyFromJMSTextMessage(textMessage,FR));
+        exchangeMessageProperties.setDFValue(extractStringPropertyFromJMSTextMessage(textMessage, DF));
+        exchangeMessageProperties.setSenderReceiver(extractStringPropertyFromJMSTextMessage(textMessage, FR));
+        exchangeMessageProperties.setOnValue(extractStringPropertyFromJMSTextMessage(textMessage, ON));
         exchangeMessageProperties.setMessageGuid(extractMessageGuidFromInputXML(textMessage.getText()));
-
         return exchangeMessageProperties;
     }
 
 
     //Extract UUID value from FLUXReportDocument as messageGuid
-    public String extractMessageGuidFromInputXML(String message){
-        String messageGuid=null;
+    public String extractMessageGuidFromInputXML(String message) {
+        String messageGuid = null;
         SAXParserForFaFLUXMessge saxParserForFaFLUXMessge = new SAXParserForFaFLUXMessge();
         try {
             saxParserForFaFLUXMessge.parseDocument(message);
         } catch (SAXException e) {
             // below message would be thrown once value is found.
-          if(e instanceof UUIDSAXException)
-            LOG.debug("************************************************");
-            messageGuid =saxParserForFaFLUXMessge.getUuidValue();
-            LOG.debug("UUID found:"+messageGuid);
-            LOG.debug("************************************************");
+            if (e instanceof UUIDSAXException)
+                log.debug("************************************************");
+            messageGuid = saxParserForFaFLUXMessge.getUuidValue();
+            log.debug("UUID found:" + messageGuid);
+            log.debug("************************************************");
         }
         return messageGuid;
     }
 
-    private String extractStringPropertyFromJMSTextMessage(TextMessage textMessage,String property){
-        String value=null;
+    private String extractStringPropertyFromJMSTextMessage(TextMessage textMessage, String property) {
+        String value = null;
         try {
             value = textMessage.getStringProperty(property);
         } catch (JMSException e) {
-            LOG.error("couldn't find the property in JMS Text Message:"+property,e);
+            log.error("Couldn't find the property [ "+property+" ] in JMS Text Message:" + property, e);
         }
         return value;
     }
